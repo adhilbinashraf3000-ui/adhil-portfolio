a401df75e0e77d893f7f8a6ea278eeb6967edfee
adhilbinashraf3000-ui <adhilbinashraf3000@gmail.com>
2026-06-04 13:43:54 +0400

    Add contact section

---
+++ src/sections/ContactSection.tsx
@@ -0,0 +1,10 @@
+import Contact from '../components/Contact';
+import Section from '../components/Section';
+
+export default function ContactSection() {
+  return (
+    <Section id="contact" eyebrow="Contact" title="Let’s connect.">
+      <Contact />
+    </Section>
+  );
+}
